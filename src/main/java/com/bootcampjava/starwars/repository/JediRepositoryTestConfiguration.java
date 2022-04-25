f9c59304d42852f67bd09deabba5c7151399b405
Mariela Atausinchi Fernandez <maratausinchi@gmail.com>
2022-04-25 00:59:10 -0300

    (repository) add jediRepositoryConfiguration

--- src/main/java/com/bootcampjava/starwars/repository/JediRepositoryTestConfiguration.java
+++ src/main/java/com/bootcampjava/starwars/repository/JediRepositoryTestConfiguration.java
@@ -1,2 +1,26 @@
-package com.bootcampjava.starwars.repository;public class JediRepositoryTestConfiguration {
+package com.bootcampjava.starwars.repository;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Primary;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+import javax.sql.DataSource;
+
+public class JediRepositoryTestConfiguration {
+
+
+    @Primary
+    @Bean
+    public DataSource dataSource() {
+
+        //setup satasource para os testes
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setDriverClassName("org.h2.Driver");
+        dataSource.setUrl("jdbc:h2:mem:db;DB_CLOSE_DELAY=1");
+        dataSource.setUsername("sa");
+        dataSource.setPassword("");
+
+        return dataSource;
+    }
+
 }
